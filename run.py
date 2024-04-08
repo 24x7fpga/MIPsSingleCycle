62947741704a90dcd084f90744ba61e77de4db1c
24x7fpga <kiran.24x7fpga@gmail.com>
2024-04-08 11:21:47 -0400

    MIPS: pythoon srcipt to simulate the design with iverilog and debug with gtkwave

---
+++ run.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import glob
+
+try:
+    os.system("iverilog -g2012 -o tb_topModule tb_topModule.sv topModule.sv mips.sv imem.sv dmem.sv alu.sv regfile.sv ctrl.sv dpath.sv")
+    print("iverilog command successful ;)")
+except:
+    print("iverilog command failed ;(")
+
+try:
+    os.system("vvp tb_topModule")
+    print("vvp command successful ;)")
+except:
+    print("vvp command failed ;(")
+
+try:
+    os.system("gtkwave tb_topModule.vcd")
+    print("gtkwave open ;)")
+except:
+    print("gtkwave failed to open ;(")
